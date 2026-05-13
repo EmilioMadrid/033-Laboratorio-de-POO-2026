c90580cfe060c2649c7bec986022fa6d7781f9f6
Emilio Madrid <emiliomad4@gmail.com>
2026-05-13 09:58:33 -0600

    Segunda clase
    Añade un programa que calcula la mascara subred por default de un prefijo CIDR

--- Practica 01/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ Practica 01/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package javaapplication1;
 
-/**
- *
- * @author emili
- */
+import java.util.Scanner;
+
 public class JavaApplication1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        
+        Scanner read = new Scanner(System.in);
+        int prefijo, octeto;
+        double[] mascara = {0, 0, 0, 0};
+        
+        do {
+            System.out.print("Introduce el prefijo CIDR: /");
+            prefijo = read.nextInt();
+
+            if (prefijo < 0 || prefijo > 32) {
+                System.out.println("El prefijo debe ser un numero entre 0 y 32");
+            }
+        } while (prefijo < 0 || prefijo > 32);
+        
+        octeto = (int) prefijo/8;
+        
+        for (int i = 0 ; i <= octeto ; i++){
+            
+            for (int j = 7 ; j >= 8-prefijo ; j--){
+                mascara[i] += Math.pow(2, j);
+            }
+            
+            prefijo -= 8;
+            
+        }
+        
+        System.out.print("La mascara por default es: ");
+        for (int i = 0 ; i <= 3 ; i++){
+            System.out.print((int)mascara[i] + ".");
+        }
+        
     }
 
 }
