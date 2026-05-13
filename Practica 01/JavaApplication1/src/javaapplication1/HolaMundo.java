775ce797ba35f44b5530c77b8b749d21583e8326
Emilio Madrid <emiliomad4@gmail.com>
2026-05-13 07:47:41 -0600

    Clase HolaMundo

---
+++ Practica 01/JavaApplication1/src/javaapplication1/HolaMundo.java
@@ -0,0 +1,14 @@
+package javaapplication1;
+
+import java.util.Date;
+
+public class HolaMundo {
+    
+    public static void main(String[] args) {
+        Date fechaActual = new Date();
+        
+        System.out.println("Nombre completo: Emilio Hernandez Madrid");
+        System.out.println("Número de matrícula: 2086253");
+        System.out.println("Fecha actual del sistema: " + fechaActual);
+    }
+}
